bf78be44816926414858ce0c6540219e1ead78e9
Elangeni <elangeniyabba96@gmail.com>
2021-02-06 22:21:06 -0500

    fixed displaying game platform/genre/store bug

--- src/Components/MainSearch/Popup.jsx
+++ src/Components/MainSearch/Popup.jsx
@@ -4,42 +4,49 @@ import './style.css';
 
 class Popup extends React.Component {
 
-  getGenre(){
+  getGenre() {
     let genres = this.props.data.get("genres");
-    
-    let arr = genres.map(x => {
-      return x.name;
-    });
-    arr.sort();
-    arr = arr.join(', ');
-    let result = "Genres: " + arr;
+    let result = "Genre: Unkown";
+
+    if (genres != null) {
+      let arr = genres.map(x => {
+        return x.name;
+      });
+      arr.sort();
+      arr = arr.join(', ');
+      result = "Genres: " + arr;
+    }
 
     return result;
   }
 
-  getStores(){
+  getStores() {
     let stores = this.props.data.get("stores");
-    if(stores.length)
-    let arr = stores.map(x => {
-      return x.store.name;
-    });
-    arr.sort();
-    
-    arr = arr.join(', ');
-    let result = "Available at: " + arr;
+    let result = "Currently unavailable for purchase.";
+    if (stores != null) {
+      let arr = stores.map(x => {
+        return x.store.name;
+      });
+      arr.sort();
+
+      arr = arr.join(', ');
+      result = "Available at: " + arr;
+    }
 
     return result;
   }
 
-  getPlatforms(){
+  getPlatforms() {
     let platforms = this.props.data.get("platforms");
-    
-    let arr = platforms.map(x => {
-      return x.platform.name;
-    });
-    arr.sort();
-    arr = arr.join(', ');
-    let result = "Available platforms: " + arr;
+    let result = "Platform data is currently unavailable."
+    if (platforms != null) {
+      let arr = platforms.map(x => {
+        return x.platform.name;
+      });
+      arr.sort();
+      arr = arr.join(', ');
+      result = "Available platforms: " + arr;
+    }
 
     return result;
   }
@@ -49,22 +56,22 @@ class Popup extends React.Component {
       <div className='popup'>
         {/* <div className='popup\_inner'> */}
 
-          <div className='slideshow' >
-            <ImageCarousel data={this.props.data} />
-          </div>
+        <div className='slideshow' >
+          <ImageCarousel data={this.props.data} />
+        </div>
 
-          <div className="game-info">
-            <h1>{this.props.data.get("name")}</h1>
-            <h3>Released On: {this.props.data.get("released")}</h3>
-            <h4>Rating: {this.props.data.get("rating")}</h4>
-            
-            <br />
-            <p>{this.getGenre()}</p>
-            <p>{this.getStores()}</p>
-            <p>{this.getPlatforms()}</p>
-            <br />
-            <button onClick={this.props.closePopup}>Back to Search</button>
-          </div>
+        <div className="game-info">
+          <h1>{this.props.data.get("name")}</h1>
+          <h3>Released On: {this.props.data.get("released")}</h3>
+          <h4>Rating: {this.props.data.get("rating")}</h4>
+
+          <br />
+          <p>{this.getGenre()}</p>
+          <p>{this.getStores()}</p>
+          <p>{this.getPlatforms()}</p>
+          <br />
+          <button onClick={this.props.closePopup}>Back to Search</button>
+        </div>
 
 
 
